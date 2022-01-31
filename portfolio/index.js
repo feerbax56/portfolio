41da1b79dd31a5df9f3b1c20905e4177e398467d
feerbax56 <94625489+feerbax56@users.noreply.github.com>
2022-01-31 22:15:26 +0500

    add: translation, ligth-theme

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,13 +1,55 @@
+import i18Obj from "./translate.js";
+
+let lang
+
+function getTranslate(lang) {
+  if (!i18Obj[lang]) return;
+  let translation = i18Obj[lang];
+  let allDataI18 = document.querySelectorAll("[data-i18n]");
+  allDataI18.forEach(function (item) {
+   const translationKey = item.getAttribute('data-i18n');
+    item.textContent = translation[translationKey];
+    if (item.placeholder) {
+      item.value = '';
+      item.placeholder = translation[translationKey];
+    }
+  });
+}
+
+let ruBtn = document.querySelector('.ru');
+let engBtn = document.querySelector('.en');
+let langBtnParent = document.querySelector('.switch-lng');
+
+langBtnParent.addEventListener('click', changeClassActiveL);
+
+function changeClassActiveL(event) {
+  if (event.target.tagName == 'BUTTON') {
+     event.target.classList.remove('active');
+    if (event.target.classList[0] === 'en') {
+      getTranslate('en');
+      lang = '.en';
+      ruBtn.classList.remove('active');
+      event.target.classList.add('active');
+    }
+    if (event.target.classList[0] === 'ru') {
+      getTranslate('ru');
+      lang = '.ru';
+      engBtn.classList.remove('active');
+      event.target.classList.add('active');
+    }
+  }
+}
+
+// hamburger menu ------------------------------------------------
+
 const hamburger = document.querySelector('.hamburger');
 const small = document.querySelector('.small-nav');
-
-
 
 function toggleMenu() {
   hamburger.classList.toggle('open');
   small.classList.toggle('open');
- 
 }
+
 hamburger.addEventListener('click', toggleMenu);
 small.addEventListener('click', closeMenu);
 
@@ -17,30 +59,119 @@ function closeMenu(event) {
     small.classList.remove('open');
   }
 }
+// самооценка ------------------------------------------------
+
+let costOfMyPain = `Итоговая оценка: 75/85.
+
+Ваша отметка - 75 балла(ов)
+Отзыв по пунктам ТЗ:
+Не выполненные/не засчитанные пункты:
+1) выбранный пользователем язык отображения страницы и светлая или тёмная тема сохраняются при перезагрузке страницы
+Комментарий проверяющего: на этом месте я заснул
+
+Частично выполненные пункты:
+1) после смены светлой и тёмной темы интерактивные элементы по-прежнему изменяют внешний вид при наведении и клике и при этом остаются видимыми на странице (нет ситуации с белым шрифтом на белом фоне) — 2 балл(а)
+Комментарий проверяющего: картинки фоновые в skills закрываются паддингом от шрифта((
+2) сложные эффекты для кнопок при наведении и/или клике — 2 балл(а)
+Комментарий проверяющего: сложная только переключение темы
 
 
-let costOfMyPain = `Итоговая оценка: 81/85.
-1. Вёрстка соответствует макету. отклонения от макета в пределах 10 px. Ширина экрана 768px +48
-  + блок <header> +6
-  + секция hero +6
-  + секция skills +6
-  + секция portfolio +6
-  + секция video +6
-  + секция price +6
-  + секция contacts +6
-  + блок <footer> +6
-2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15
-  + нет полосы прокрутки при ширине страницы от 1440рх до 768рх +5
-  + нет полосы прокрутки при ширине страницы от 768рх до 480рх +5
-  + нет полосы прокрутки при ширине страницы от 480рх до 320рх +5
-3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22
-  + при ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка +2
-  + при нажатии на бургер-иконку справа появляется адаптивное меню, бургер-иконка изменяется на крестик +2 (пришлось пожертвовать плавностью выезда меню, так как появлялась полоса прокрутки и меню не уходило в displey:none)
-  + высота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4
-  + при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку +2(про плавность уже говорил)
-  + бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений +2
-  + ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +2
-  + при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку +4
+Все оставшиеся пункты выполнены и не имеют комментариев проверяющего.
+
 `;
 
 console.log(costOfMyPain);
+
+// portfolio image ----------------------------------------------
+
+// const portfolioBtn = document.querySelectorAll('.buttonPortfolio');
+const portfolioBtns = document.querySelectorAll('.portfolio-btn');
+const portfolioImages = document.querySelectorAll('.portfolio');
+
+portfolioBtns[2].classList.add('active');
+
+portfolioBtns.forEach((el) => el.addEventListener('click', changeImagesSeason));
+
+function changeImagesSeason(event) {
+  let dataSeason = event.target.dataset.season;
+  portfolioImages.forEach((img, index) => img.src = `./assets/img/${dataSeason}/${index + 1}.jpg`);
+}
+
+for (let i = 0; i < portfolioBtns.length; i++) {
+  portfolioBtns[i].addEventListener('click', changeClassActive);
+}
+
+function changeClassActive() {
+  for (let i = 0; i < portfolioBtns.length; i++) {
+    portfolioBtns[i].classList.remove('active');
+  }
+  this.classList.add('active');
+}
+
+// Cash images---------------------------------------------
+
+
+function preloadImages() {
+  const seasons = ['winter', 'spring', 'summer', 'autumn'];
+  seasons.forEach(function (el) {
+    for (let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `./assets/img/${el}/${i}.jpg`;
+    }
+  });
+}
+preloadImages();
+
+//switch-theme-----------------------------------------------
+
+const themeButton = document.querySelector('.switch-theme');
+const lightItem = document.querySelectorAll('.ligth')
+const whiteButton = document.querySelectorAll('.portfolio-btn')
+
+function changeColor() {
+  
+  themeButton.classList.toggle('white')
+  lightItem.forEach(item => item.classList.toggle('ligth-theme'))
+  whiteButton.forEach(item => item.classList.toggle('button-ligth-theme'))
+
+  const persistTheme =!!localStorage.getItem('theme')
+  const isReload = localStorage.getItem('isReload')
+
+  if (!isReload && persistTheme) {
+    localStorage.setItem("theme", "")
+  } else {
+    localStorage.setItem("theme", "ligth-theme")
+  }
+  localStorage.setItem("isReload", "")
+ 
+}
+
+themeButton.addEventListener("click", changeColor)
+
+//local storage---------------------------
+
+
+//--funny btn---------------------
+
+
+const button = document.querySelector('.ripple')
+
+button.addEventListener('click', function (e) {
+  const x = e.clientX
+  const y = e.clientY
+
+  const buttonTop = e.target.offsetTop
+  const buttonLeft = e.target.offsetLeft
+
+  const xInside = x - buttonLeft
+  const yInside = y - buttonTop
+
+  const circle = document.createElement('span')
+  circle.classList.add('circle')
+  circle.style.top = yInside + 'px'
+  circle.style.left = xInside + 'px'
+
+  this.appendChild(circle)
+
+  setTimeout(() => circle.remove(), 500)
+})
